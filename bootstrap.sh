b9d0aa214f3b81b5fbbcf251a1e990599952676b
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-01-12 20:56:24 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+/home/niklas/Projekte/vmdebootstrap/vmdebootstrap \
+    --arch armhf \
+    --variant minbase \
+    --distribution jessie \
+    --mirror http://[::1]:3142/archive.raspbian.org/raspbian \
+    --image `date +raspbian-%Y%m%d.img` \
+    --keyring `pwd`/raspbian.org.gpg \
+    --size 2000M \
+    --bootsize 64M \
+    --boottype vfat \
+    --root-password raspberry \
+    --enable-dhcp \
+    --verbose \
+    --no-kernel \
+    --no-extlinux \
+    --hostname raspberry \
+    --foreign /usr/bin/qemu-arm-static \
+    --customize `pwd`/customize.sh
---
+++ customize.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -e
+
+ROOTDIR="$1"
+
+# Do not start services during installation.
+echo exit 101 > $ROOTDIR/usr/sbin/policy-rc.d
+chmod +x $ROOTDIR/usr/sbin/policy-rc.d
+
+# Configure apt.
+export DEBIAN_FRONTEND=noninteractive
+cat raspbian.org.gpg | chroot $ROOTDIR apt-key add -
+mkdir -p $ROOTDIR/etc/apt/sources.list.d/
+mkdir -p $ROOTDIR/etc/apt/apt.conf.d/
+echo "Acquire::http { Proxy \"http://[::1]:3142\"; };" > $ROOTDIR/etc/apt/apt.conf.d/50apt-cacher-ng
+cp etc/apt/sources.list $ROOTDIR/etc/apt/sources.list
+cp etc/apt/apt.conf.d/50raspi $ROOTDIR/etc/apt/apt.conf.d/50raspi
+chroot $ROOTDIR apt-get update
+
+# Regenerate SSH host keys on first boot.
+chroot $ROOTDIR apt-get install -y openssh-server
+cp etc/rc.local $ROOTDIR/etc/rc.local
+chmod a+x $ROOTDIR/etc/rc.local
+chroot $ROOTDIR update-rc.d rc.local defaults
+
+# Configure.
+cp boot/cmdline.txt $ROOTDIR/boot/cmdline.txt
+cp boot/config.txt $ROOTDIR/boot/config.txt
+cp etc/fstab $ROOTDIR/etc/fstab
+cp etc/modules $ROOTDIR/etc/modules
+
+# Install kernel.
+mkdir -p $ROOTDIR/lib/modules
+chroot $ROOTDIR apt-get install -y ca-certificates curl binutils git-core kmod
+wget https://raw.github.com/Hexxeh/rpi-update/master/rpi-update -O $ROOTDIR/usr/local/sbin/rpi-update
+chmod a+x $ROOTDIR/usr/local/sbin/rpi-update
+SKIP_BACKUP=1 ROOT_PATH=$ROOTDIR BOOT_PATH=$ROOTDIR/boot $ROOTDIR/usr/local/sbin/rpi-update
+
+# Install extra packages.
+chroot $ROOTDIR apt-get install -y vim-tiny netbase less iputils-ping net-tools
+chroot $ROOTDIR apt-get install -y anacron fake-hwclock
+
+# Install other recommended packages.
+#apt-get install ntp apt-cron fail2ban
+
+# Create a swapfile.
+#dd if=/dev/zero of=$ROOTDIR/var/swapfile bs=1M count=512
+#chroot $ROOTDIR mkswap /var/swapfile
+#echo /var/swapfile none swap sw 0 0 >> $ROOTDIR/etc/fstab
+
+# Done.
+rm $ROOTDIR/usr/sbin/policy-rc.d
+rm $ROOTDIR/etc/apt/apt.conf.d/50apt-cacher-ng
