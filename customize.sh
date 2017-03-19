1e57a36cfdfcccafff1b7a9243012d8b09a15c27
Valentin Lorentz <progval+git@progval.net>
2017-03-19 11:29:37 +0100

    Fix generation of ssh keys.
    * Use systemd unit instead of rc.local, which seems to have
      been broken by a Debian update somehow.
    * Run rng-tools to use /dev/hwrng before generating keys.

--- customize.sh
+++ customize.sh
@@ -20,18 +20,19 @@ chroot $ROOTDIR apt-get update
 
 # Regenerate SSH host keys on first boot.
 chroot $ROOTDIR apt-get install -y openssh-server
-cp etc/rc.local $ROOTDIR/etc/rc.local
-chmod a+x $ROOTDIR/etc/rc.local
 rm -f $ROOTDIR/etc/ssh/ssh_host_*
-chroot $ROOTDIR update-rc.d rc.local defaults
+mkdir -p $ROOTDIR/etc/systemd/system
+cp $SOURCEDIR/etc/systemd/system/regen-ssh-keys.service $ROOTDIR/etc/systemd/system/regen-ssh-keys.service
+chroot $ROOTDIR systemctl enable regen-ssh-keys
 
 # Configure.
 cp $SOURCEDIR/boot/cmdline.txt $ROOTDIR/boot/cmdline.txt
 cp $SOURCEDIR/boot/config.txt $ROOTDIR/boot/config.txt
+cp -r $SOURCEDIR/etc/default $ROOTDIR/etc/default
 cp $SOURCEDIR/etc/fstab $ROOTDIR/etc/fstab
 cp $SOURCEDIR/etc/modules $ROOTDIR/etc/modules
-cp $SOURCEDIR/etc/ssh/sshd_config $ROOTDIR/etc/ssh/sshd_config
 cp $SOURCEDIR/etc/network/interfaces $ROOTDIR/etc/network/interfaces
+sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/" $ROOTDIR/etc/ssh/sshd_config
 
 # Install kernel.
 mkdir -p $ROOTDIR/lib/modules
@@ -42,7 +43,7 @@ SKIP_WARNING=1 SKIP_BACKUP=1 ROOT_PATH=$ROOTDIR BOOT_PATH=$ROOTDIR/boot $ROOTDIR
 
 # Install extra packages.
 chroot $ROOTDIR apt-get install -y apt-utils vim nano whiptail netbase less iputils-ping net-tools isc-dhcp-client man-db
-chroot $ROOTDIR apt-get install -y anacron fake-hwclock
+chroot $ROOTDIR apt-get install -y anacron fake-hwclock rng-tools
 
 # Install other recommended packages.
 #apt-get install ntp apt-cron fail2ban needrestart
